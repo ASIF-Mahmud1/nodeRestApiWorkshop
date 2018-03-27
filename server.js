6e9ed9807ec3c1fcaff5950522f05ce797c3abf6
shamiul hoque <shoq9876@gmail.com>
2018-03-27 16:06:04 +0600

    express hello world created

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
+
+const express = require('express');
+const app = express();
 const article = require("./model/article-model.js");
+const articleRoute = require("./route/article-route.js");
 // Connection URL
 const url = 'mongodb://localhost:27017';
 
@@ -12,12 +16,18 @@ MongoClient.connect(url, function(err, client) {
   assert.equal(null, err);
   console.log("Connected successfully to server");
   
+
+
   const db = client.db(dbName);
-  article.createArticle(db,{username:"abul",title:"title1",content: "hello wolrd",creationDate:Date.now() },function(result){
+  articleRoute(app);
+  app.listen(3000, () => console.log('Example app listening on port 3000!'))
+  
 
-    console.log("created succesfully");
+//   article.createArticle(db,{username:"abul",title:"title1",content: "hello wolrd",creationDate:Date.now() },function(result){
 
-  });
+//     console.log("created succesfully");
+
+//   });
 
   client.close();
 });
